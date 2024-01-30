990bffb6f8433cac79f447f322f9b1201e37a3f8
Anjali Tapare <tapareanjali1122@gamil.com>
2024-01-30 19:05:46 +0530

    create pages and push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/forgotpassword";
+import Offers from "./pages/offers";
+import SignIn from "./pages/signin";
+import SignUp from "./pages/signup";
+
+
+
+
+
 
 function App() {
   return (
     <>
-      <h1 > love you jessus </h1>
+      
+      <Router>
+        <Routes>
+          <Route  path="/"element={<Home/>}/>
+          <Route  path="/ profile"element={<profile/>}/>
+          <Route  path="/sign-in"element={<SignIn/>}/>
+          <Route  path="/ sign-up"element={<SignUp/>}/>
+          <Route  path="/ forgot-password"element={<ForgotPassword/>}/>
+          <Route  path="/ offers"element={<Offers/>}/>
+          
+        </Routes>
+      </Router>
 
     </>
     
